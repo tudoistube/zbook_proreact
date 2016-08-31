67cef529ddf461c137a58b4c8be887719151849c
tudoistube <tudoistube@ymail.com>
2016-08-31 15:37:57 +0900

    by_80p

---
+++ app/CheckList.js
@@ -0,0 +1,30 @@
+import React, { Component, PropTypes } from 'react';
+
+class CheckList extends Component {
+  render() {
+    let tasks = this.props.tasks.map((task) => (
+      <li key={task.id} className="checklist__task">
+    {/* 체크박스 : 제어컴포넌트임*/}
+        <input type="checkbox" defaultChecked={task.done} />
+        {task.name}{' '}
+        <a href="#" className="checklist__task--remove" />
+      </li>
+    ));
+
+    return (
+      <div className="checklist">
+        <ul>{tasks}</ul>
+    {/* 입력상자 : 비제어컴포넌트임*/}        
+        <input type="text" className="checklist--add-task"
+                placeholder="입력상자 : 비제어컴포넌트임" />
+      </div>
+    );
+  }
+};
+
+CheckList.propTypes = {
+  cardId: PropTypes.number,
+  tasks: PropTypes.arrayOf(PropTypes.object)
+};
+
+export default CheckList;
---
+++ app/FocusText_65p_ref.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {render} from 'react-dom';
+
+class FocusText_65p_ref extends Component {
+
+	handleClick() {
+	
+		this.refs.myTextInput.focus();
+	}
+
+	render() {
+	
+		return( 
+			<div>
+				<input type="text" ref="myTextInput" />
+				<input type="button"
+								value="Focus the text input"
+								onClick={this.handleClick.bind(this)} />
+			</div>
+			);
+	}
+}
+
+render(<FocusText_65p_ref />, document.getElementById('root'));
