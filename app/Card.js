bd749c61f318c363dfe8217b6124adc8fce46241
2DoIs2Be <tudoistube@ymail.com>
2016-09-04 19:17:08 +0900

    by_37p

--- app/Card.js
+++ app/Card.js
@@ -1,3 +1,4 @@
+//...36p.
 import React, { Component } from 'react';
 import CheckList from './CheckList';
 
@@ -10,11 +11,20 @@ class Card extends Component {
     }
   };
 
+/*
+31p에서 굵은 화살표('( )=>')를 이용해서 onClick 이벤트 핸들러 안에 인라인 함수로
+추가하는 방법은 유연하지 않아서, 클래스 안에서 toggleDetails라는 새로운 메서드를
+이용해 이벤트를 처리함.
+
+현재 리액트 버전에서는 개발자가 함수를 명시적으로 컨텍스트에 바인딩해야 함.
+이를 위해 .bind(this)를 이용해 바인딩된 함수를 만드는 방법이 가장 간단함.
+ES6클래스 사용되기 전에는 모든 메서드가 자동으로 바인딩되었으나 제거됨.
+*/
   toggleDetails(){
     this.setState(
       {showDetails: !this.state.showDetails}
     );
-  }  
+  }
 
   render() {
     let cardDetails;
@@ -32,7 +42,7 @@ class Card extends Component {
 
     return (
       <div className="card">
-        <div className="card__title" 
+        <div className="card__title"
              onClick={this.toggleDetails.bind(this)}>
           {this.props.title}
         </div>
