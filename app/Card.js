b836cffc7d95c77f44ed3f641920766a60a70775
2DoIs2Be <tudoistube@ymail.com>
2016-09-04 16:45:11 +0900

    by_32p

--- app/Card.js
+++ app/Card.js
@@ -1,5 +1,14 @@
+//...29p.
 import React, { Component } from 'react';
 import CheckList from './CheckList';
+
+/*
+1. showDetails라는 키를 컴포넌트의 상태로 정의하는 생성자 함수 추가.
+
+2. render()의 JSX를 수정해 showDetails 상태 속성이 true일 때만 카드 세부사항을 렌더링함.
+
+3. 클릭 이벤트 핸들러를 추가해서 showDetails를 토글함.
+*/
 
 class Card extends Component {
 
@@ -9,7 +18,7 @@ class Card extends Component {
       showDetails: false
     }
   };
-  
+
 
   render() {
     let cardDetails;
@@ -26,7 +35,7 @@ class Card extends Component {
 
     return (
       <div className="card">
-        <div className="card__title" 
+        <div className="card__title"
              onClick={()=>this.setState({showDetails: !this.state.showDetails})}>
           {this.props.title}
         </div>
